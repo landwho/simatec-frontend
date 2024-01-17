619d39382dec43c71953ab34bfb03803d65794cb
jubico0 <jubico@labymed.com.gt>
2024-01-17 09:22:57 -0600

    first commit

--- src/app/Modules/login/login.component.ts
+++ src/app/Modules/login/login.component.ts
@@ -9,4 +9,25 @@ export class LoginComponent {
 
 constructor(){}
 
+
+cities!: City[];
+
+selectedCity!: City;
+
+ngOnInit() {
+    this.cities = [
+        { name: 'New York', code: 'NY' },
+        { name: 'Rome', code: 'RM' },
+        { name: 'London', code: 'LDN' },
+        { name: 'Istanbul', code: 'IST' },
+        { name: 'Paris', code: 'PRS' }
+    ];
+}
+
+}
+
+
+interface City {
+  name: string,
+  code: string
 }
--- src/app/Modules/login/login.module.ts
+++ src/app/Modules/login/login.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login-routing';
 
+import { FormsModule } from '@angular/forms';
+
+
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
     CommonModule,
+    FormsModule,
     LoginRoutingModule,
+
   ]
 })
 export class LoginModule { }
